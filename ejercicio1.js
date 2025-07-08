549e273f29f1360554562d5c2615822380f0c6f2
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-08 20:09:31 +0200

    ejercicio 1 completado

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,5 +1,15 @@
-//1.1
-const x = 10;
+//1.1 - Define tu superhéroe favorito
+const miSuperheroeFavorito = "Hulk";
 
-//1.2
-const y = x + 10;
+//1.2 - Definir un número
+const miNumeroFavorito = 50;
+
+//1.3 Definir las dimensiones
+const altura = 5;
+const longitud = 10;
+
+//1.4 Calcular la suma
+const suma = altura + longitud;
+
+//1.5 Imprimir los resultados
+console.log(suma);
