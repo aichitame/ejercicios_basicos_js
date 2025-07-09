f663b8c9339799fc8fae53a9c66f555aa4f1098c
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-09 13:38:02 +0200

    ejercicio 11 realizado

---
+++ ejercicio11.js
@@ -0,0 +1,20 @@
+//Calcular promedio mezclado: Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume.
+
+const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
+
+function averageWord(list) {
+  let total = 0;
+
+  for(let i = 0; i < list.length; i++){
+    //const element = mixedElements[i];
+
+    if(typeof list[i] === 'number'){
+        total += list[i];
+    } else if (typeof list[i] === 'string'){
+    total += list[i].length;
+  }
+}
+  return total;
+}
+
+console.log(averageWord(mixedElements));
