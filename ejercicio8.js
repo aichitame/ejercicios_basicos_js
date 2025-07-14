5495bed80c0e0cfbf45cb2cb09741f11ad535650
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-14 19:16:34 +0200

    ejercicio 8 corregido

--- ejercicio8.js
+++ ejercicio8.js
@@ -4,16 +4,16 @@
 const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
 
 function findLongestWord(stringList) {
-  let longest = stringList[0];
+  let longestWord = "";
 
-  for(let i = 1; i < stringList.length; i++){
-    if(stringList[i].length > longest.length){
-      longest = stringList[i];
+  for (const string of stringList) {
+    if (string.length > longestWord.length) {
+      longestWord = string;
     }
   }
-
-  return longest;
+  console.log(longestWord);
 }
 
-console.log(findLongestWord(avengers));
+findLongestWord(avengers);
+
 
