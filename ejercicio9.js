b70d2fd4e2daf185c36c0be36086222bfe365824
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-14 19:25:27 +0200

    ejercicio 9 actualizado

--- ejercicio9.js
+++ ejercicio9.js
@@ -1,6 +1,7 @@
 //Completa la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números del array.
 
 const numbers = [1, 2, 3, 5, 45, 37, 58];
+const newNumbers = [56, 77, 71, 23, 44, 51, 87, 22, 49]
 
 function sumNumbers(numberList) {
 let sum = 0;
@@ -11,6 +12,7 @@ console.log(sum);
 }
 
 sumNumbers(numbers);
+sumNumbers(newNumbers)
 
 /* OPCIÓN B
 
