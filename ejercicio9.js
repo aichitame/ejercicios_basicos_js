3883024d0fd9c799c8bed3184f09a2928e330c84
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-14 19:21:48 +0200

    ejercicio 9 actualizado

--- ejercicio9.js
+++ ejercicio9.js
@@ -4,6 +4,15 @@ const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumNumbers(numberList) {
 let sum = 0;
+for (const number of numberList) {
+    sum += number;
+}
+console.log(sum);
+}
+
+sumNumbers(numbers);
+
+/* OPCIÓN B
 
 for(let i = 0; i < numberList.length; i++){
     sum += numberList[i];
@@ -12,3 +21,4 @@ return sum;
 }
 
 console.log(sumNumbers(numbers));
+*/
