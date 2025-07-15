7882ab64668cd40dfe83a377b729671b46ee978f
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-15 12:27:39 +0200

    ejercicio 13 hecho

--- ejercicio13.js
+++ ejercicio13.js
@@ -1,3 +1,5 @@
+//Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. Esta función comprueba si existe el elemento, y en caso de que exista nos devuelve un true y la posición de dicho elemento y por la contra un false.
+
 const names = [
     'Peter',
     'Steve',
@@ -11,6 +13,31 @@ const names = [
     'Jessica',
     'Marc'
   ];
-  function nameFinder(nameList) {
-    // Completar
+
+  function searchName(array, value) {
+    const index = array.indexOf(value);
+    if (index !== -1) {
+      return true + ' - posición: ' + index;
+    } else {
+      return false;
+    }
   }
+  
+  const result1 = searchName(names, 'María');
+console.log(result1); // true - posición: 2
+
+const result2 = searchName(names, 'Aixa');
+console.log(result2); // false
+
+
+  /* Esto es lo que he podido yo razonar sin ayuda :')
+  
+  function nameFinder(nameList) {
+    for (const name of nameList) {
+      if(nameList.includes(name)){
+        console.log(name);
+      }
+    }
+  }
+
+  nameFinder(names) */
