d7ab93710327f1b2652d1b23701a8bb219b64eb2
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-15 14:10:27 +0200

    ejercicio 22 hecho

--- ejercicio22.js
+++ ejercicio22.js
@@ -1,4 +1,7 @@
-const fruits = ["Strawberry", "Banana", "Orange", "Apple"];
+//Usa un for para remplazar todas las comidas que no sean veganas con las frutas del array de frutas. Recuerda no usar frutas duplicadas.
+
+const fruits = ["Strawberry", "Banana", "Orange", "Apple", "Pineapple"];
+
 
 const foodSchedule = [
   { name: "Heura", isVegan: true },
@@ -7,4 +10,19 @@ const foodSchedule = [
   { name: "Burger", isVegan: false },
   { name: "Rice", isVegan: true },
   { name: "Pasta", isVegan: true },
+  { name: "Beef", isVegan: false},
+  { name: "Buñuelos", isVegan: false},
+  { name: "Carne de res", isVegan: false}
 ];
+
+let i = 0;
+
+for(const food of foodSchedule){
+  if(!food.isVegan){
+    food.name = fruits[i];
+    food.isVegan = true;
+    i++;
+  }
+}
+
+console.log(foodSchedule);
