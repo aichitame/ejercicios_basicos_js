d67c6da4efd15cdcd92984b95443c54eeed39526
aichitame <159391105+aichitame@users.noreply.github.com>
2025-07-15 14:27:47 +0200

    ejercicio 8 completado

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,5 +1,5 @@
 
-// Buscar la palabra más larga: Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+//Buscar la palabra más larga: Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
 
 const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
 
